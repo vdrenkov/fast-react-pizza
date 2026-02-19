eac949028ef863ca4fc0786bffeac91be7c9d8d3
Valentin Drenkov <drenkovvv@gmail.com>
2026-02-19 19:21:43 +0200

    Warning cleared.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15,7 +15,7 @@ export function formatDate(dateStr) {
 }
 
 export function calcMinutesLeft(dateStr) {
-  const d1 = new Date().getTime();
+  const d1 = Date.now();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
 }
